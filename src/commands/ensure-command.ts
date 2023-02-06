03750ba8377ed50c884442bceb730d4dbaabb5e7
maxider <maxweilandt1998@gmail.com>
2023-02-07 00:24:33 +0100

    fix: added cache to ensure-command.ts

--- src/commands/ensure-command.ts
+++ src/commands/ensure-command.ts
@@ -15,10 +15,15 @@ export async function ensureCommand(
   for (const guild of client.guilds.cache.values()) {
     if (typeof command === "function") command = await command(guild);
     const name = (command as BaseApplicationCommandData).name;
-    const commands = await guild.commands.fetch();
-    const match = commands.find(it => it.name === name);
-    if (match) return;
-    if (match) await guild.commands.delete(match);
+    //Check if in cache
+    let commands = await guild.commands.cache;
+    const cacheMatch = commands.find(it => it.name === name);
+    if (cacheMatch) return;
+    //Check again for fetch
+    commands = await guild.commands.fetch();
+    const fetchMatch = commands.find(it => it.name === name);
+    if (fetchMatch) return;
+    //Command does not exist in cache nor in fetch. create it
     await guild.commands.create(command);
   }
 }
