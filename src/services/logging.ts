653d0bf72b986821e651335e725af2ebc48769b2
Tim Horlacher <tim.horlacher@protonmail.com>
2023-02-07 13:45:11 +0100

    Added roles to modlog

--- src/services/logging.ts
+++ src/services/logging.ts
@@ -6,20 +6,69 @@ import {
   GuildAuditLogsEntry,
   GuildBan,
   Message,
+  Role,
 } from "discord.js";
 import {
   Handler,
   OnBanCreate,
   OnBanDelete,
-  OnMessageCreate,
   OnMessageDelete,
   OnMessageUpdate,
+  OnRoleCreate,
+  OnRoleDelete,
+  OnRoleUpdate,
 } from "src/decorators";
 import { modLog } from "src/logging/mod-log";
 import { findChannel } from "src/messages";
 
 @Handler()
 export class LoggingService {
+  @OnRoleCreate()
+  async onRoleCreate(role: Role) {
+    const fetchedLogs = await role.guild.fetchAuditLogs({
+      type: AuditLogEvent.RoleCreate,
+      limit: 1,
+    });
+    const roleLog: GuildAuditLogsEntry = fetchedLogs.entries.first();
+    await modLog(role.guild, {
+      content: `Die Rolle **${role.name}** wurde von **${roleLog.executor.username}#${roleLog.executor.discriminator}** erstellt`,
+      components: [],
+    });
+  }
+
+  @OnRoleUpdate()
+  async onRoleUpdate(oldRole: Role, newRole: Role) {
+    const fetchedLogs = await newRole.guild.fetchAuditLogs({
+      type: AuditLogEvent.RoleUpdate,
+      limit: 1,
+    });
+    const roleLog: GuildAuditLogsEntry = fetchedLogs.entries.first();
+    if (oldRole.name === newRole.name) {
+      await modLog(newRole.guild, {
+        content: `Die Rolle **${oldRole.name}** wurde von **${roleLog.executor.username}#${roleLog.executor.discriminator}** verändert`,
+        components: [],
+      });
+    } else {
+      await modLog(newRole.guild, {
+        content: `Die Rolle **${oldRole.name}** wurde von **${roleLog.executor.username}#${roleLog.executor.discriminator}** zu **${newRole.name}** geändert`,
+        components: [],
+      });
+    }
+  }
+
+  @OnRoleDelete()
+  async onRoleDelete(role: Role) {
+    const fetchedLogs = await role.guild.fetchAuditLogs({
+      type: AuditLogEvent.RoleDelete,
+      limit: 1,
+    });
+    const roleLog: GuildAuditLogsEntry = fetchedLogs.entries.first();
+    await modLog(role.guild, {
+      content: `Die Rolle **${role.name}** wurde von **${roleLog.executor.username}#${roleLog.executor.discriminator}** gelöscht`,
+      components: [],
+    });
+  }
+
   @OnBanCreate()
   async onBanCreate(ban: GuildBan) {
     const fetchedLogs = await ban.guild.fetchAuditLogs({
