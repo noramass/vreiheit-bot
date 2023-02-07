c23f2ab82da30e48d41c9a388feb8b5a943534b9
Tim Horlacher <tim.horlacher@protonmail.com>
2023-02-07 13:26:01 +0100

    Added bans to modlog

--- src/services/logging.ts
+++ src/services/logging.ts
@@ -1,11 +1,16 @@
 import {
   ActionRowBuilder,
+  AuditLogEvent,
   ButtonBuilder,
   ButtonStyle,
+  GuildAuditLogsEntry,
+  GuildBan,
   Message,
 } from "discord.js";
 import {
   Handler,
+  OnBanCreate,
+  OnBanDelete,
   OnMessageCreate,
   OnMessageDelete,
   OnMessageUpdate,
@@ -15,16 +20,46 @@ import { findChannel } from "src/messages";
 
 @Handler()
 export class LoggingService {
-  @OnMessageCreate()
-  async onMessageCreate(message: Message) {
-    if (!message.author.bot) {
-      let channel = await findChannel(message.guild, message.channelId);
-      await modLog(message.guild, {
-        content: `In **${channel.name}** schrieb **${message.author.username}#${message.author.discriminator}**: "${message.content}"`,
-        components: [this.buildUserActionRow(message)],
-      });
-    }
+  @OnBanCreate()
+  async onBanCreate(ban: GuildBan) {
+    const fetchedLogs = await ban.guild.fetchAuditLogs({
+      type: AuditLogEvent.MemberBanAdd,
+      limit: 1,
+    });
+    const banLog: GuildAuditLogsEntry = fetchedLogs.entries.first();
+    await modLog(ban.guild, {
+      content: `**${ban.user.username}#${
+        ban.user.discriminator
+      }** wurde von **${banLog.executor.username}#${
+        banLog.executor.discriminator
+      }** gebannt: **${banLog.reason ?? "N/A"}**`,
+      components: [],
+    });
   }
+
+  @OnBanDelete()
+  async onBanDelete(unban: GuildBan) {
+    const fetchedLogs = await unban.guild.fetchAuditLogs({
+      type: AuditLogEvent.MemberBanRemove,
+      limit: 1,
+    });
+    const unbanLog: GuildAuditLogsEntry = fetchedLogs.entries.first();
+    await modLog(unban.guild, {
+      content: `**${unban.user.username}#${unban.user.discriminator}** wurde von **${unbanLog.executor.username}#${unbanLog.executor.discriminator}** entbannt`,
+      components: [],
+    });
+  }
+
+  //   @OnMessageCreate()
+  //   async onMessageCreate(message: Message) {
+  //     if (!message.author.bot) {
+  //       let channel = await findChannel(message.guild, message.channelId);
+  //       await modLog(message.guild, {
+  //         content: `In **${channel.name}** schrieb **${message.author.username}#${message.author.discriminator}**: "${message.content}"`,
+  //         components: [this.buildUserActionRow(message)],
+  //       });
+  //     }
+  //   }
 
   @OnMessageUpdate()
   async onMessageUpdate(oldMessage: Message, newMessage: Message) {
