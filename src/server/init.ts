88556cd685816d1e91f7da9c9d35df7d7d25c0f8
Nora Reichwald <nora@reichwald.me>
2023-02-11 16:35:22 +0100

    chore: add / handler for certmanager

--- src/server/init.ts
+++ src/server/init.ts
@@ -6,5 +6,6 @@ export const api = express();
 export async function initApi(invite: string) {
   api.get("/ping", (req, res) => res.send("pong!"));
   api.get("/invite", (req, res) => res.redirect(invite));
+  api.get("/", (req, res) => res.send("Hello World!"));
   api.listen(env("api_port", 3000), () => console.log("api up!"));
 }
