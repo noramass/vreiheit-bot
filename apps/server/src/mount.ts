ff2c212a12a18fa401fb757e7c3dfc57fa47ae5d
Nora Reichwald <nora@reichwald.me>
2023-04-26 22:17:57 +0200

    fix: typing for api constant

--- apps/server/src/mount.ts
+++ apps/server/src/mount.ts
@@ -1,8 +1,8 @@
 import { createMount } from "@propero/easy-api";
 import { createInjectionDecorators } from "@vreiheit/util";
-import { Router } from "express";
+import { IRouter, Router } from "express";
 
-export const api = Router();
+export const api: IRouter = Router();
 export const HttpController = createMount(api);
 
 export const { Inject, Injectable } = createInjectionDecorators();
