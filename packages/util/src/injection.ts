0f842e778aff2b9d54a900e45f65dad3d0e68964
Nora Reichwald <nora@reichwald.me>
2023-05-02 09:08:39 +0200

    feat: injection factories and initialisation options

--- packages/util/src/injection.ts
+++ packages/util/src/injection.ts
@@ -1,11 +1,46 @@
-export function createInjectionDecorators() {
-  const registered = new Map<any, any>();
-  function Injectable(Cls: any) {
-    return class extends Cls {
-      constructor(...params: any[]) {
-        super(...params);
-        registered.set(Cls, this);
+export interface InjectableOptions<T> {
+  initialise?: boolean;
+  factory?: () => T;
+}
+
+export type Constructor<T> = new (...args: any) => T;
+
+export type InjectableDecorator = <T>(
+  options?: InjectableOptions<T>,
+) => (Cls: Constructor<T>) => Constructor<T>;
+export type InjectPropertyDecorator = <T>(
+  cls: () => Constructor<T>,
+) => (
+  proto: any,
+  propertyKey: string | symbol,
+  desc?: TypedPropertyDescriptor<T>,
+) => void;
+
+export interface InjectionDecorators {
+  Injectable: InjectableDecorator;
+  Inject: InjectPropertyDecorator;
+}
+
+function cache<T>(fn: () => T): () => T {
+  let cached: T | undefined;
+  return () => (cached ??= fn());
+}
+
+export function createInjectionDecorators(): InjectionDecorators {
+  const registered = new Map<any, () => any>();
+  function Injectable({ initialise, factory }: InjectableOptions<any> = {}) {
+    return function (Cls: any) {
+      if (initialise) factory = () => new Cls();
+      if (factory) {
+        registered.set(Cls, cache(factory));
+        return Cls;
       }
+      return class extends Cls {
+        constructor(...params: any[]) {
+          super(...params);
+          registered.set(Cls, () => this);
+        }
+      };
     };
   }
 
